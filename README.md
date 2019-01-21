624c2525dc27ad03143c92fa094c82c7fe818371
Marnix Kok <marnix@xinsolutions.co.nz>
2019-01-22 11:02:56 +1300

    Changes:
    * initial commit

---
+++ README.md
@@ -0,0 +1,230 @@
+# Swagger Scraper
+
+The purpose of this module is to generate Swagger documentation from the JSDoc tags you have annotated your endpoint handlers with. It is able to interpret complex types as well.
+
+Feedback is welcome. 
+
+## How to initialise
+
+You'll need an ExpressJS application. Find the point at which you have initialised the endpoints.
+
+Required packages:
+
+* express-list-endpoints: ^4.0.0
+* swagger-ui-express: ^4.0.2
+
+The following code will initialise the swagger documentation endpoints:
+
+	
+	// assumed: app = the express js application instance
+
+	// include this dependency
+	const swaggerScrape = require('swagger-scrape');
+
+	// grab all endpoint information of endpoints registered with express js
+	let swaggerEndpoints = swaggerScrape.scrapeEndpoints("./", app);
+
+	// conver the endpoint information to a swagger document
+	let swaggerDoc = swaggerScrape.toSwaggerJson(appInfo, "hostname", "/application-context", swaggerEndpoints);
+
+	// expose the swagger ui based on the swagger doc at '/api-docs'.
+	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+
+## Annotating your code
+
+What follows is a completely annotated function that produces a proper swagger document:
+
+
+	/**
+	 * @swagger
+	 *
+	 * Just put the complete description here. It is the description that is shown when you click into a 
+	 * swagger endpoint in the interface.
+	 *
+	 * @summary This is the short text shown right next to the endpoint name
+	 * @tag TestEndpoint
+	 *
+	 * @param req       the request object
+	 * @param resp      the reponse object
+	 *
+	 * @param pathParameter (path) {integer} part of the url
+	 * @param queryParam	(query) {string} part of the requestquery (?queryParam=blaat)
+	 * @param headerParam	(header) {string} expected header value from request headers
+	 * @param bodyModel 	(body) {ExpectedModel} This is a complex type parameter that has a proper type hint.
+	 *
+	 * @response 403    You cannot go here.
+	 * @response 200    {ExpectedModel} all is well, information is returned in complex type.
+	 */
+	module.exports = function(req, resp) {
+
+		// your code here.
+
+	};
+
+
+
+### Basics
+
+To indicate that this jsdoc pertains to swagger documentation, add the `@swagger` tag.
+
+You can organise your swagger endpoints into different categories by specifying one or more `@tag`.
+
+Specify a short summary by adding the `@summary` tag. 
+
+### Parameters
+
+Parameters have the following notation:
+
+	@param <name> (query|path|header|body) {type} description
+
+You can add an asterisk in front of the parameter type to indicate that it is required, like: 
+	
+	@param myParam *(query) {string} a required parameter
+
+If you do not specify a type, `string` is assumed.
+
+The following primitive types are supported:
+
+* number
+* boolean
+* integer
+* string
+* object
+* file 
+
+Anything else is considere to be a complex type and has additional logic to retrieve information for.
+
+### Response
+
+Specifying a swagger response can be done by using the following notation:
+
+	@response <statusCode> {type} description
+
+If no type is specified `string` is assumed. You can specify a complex type here as well.
+
+### Complex Types
+
+When you specify a type name that is not a primitive, the swagger scraper uses the same file to try and find a constant of the name specified as the type name that was annotated with a `@model` jsdoc tag. 
+
+Fields inside the constant definition can be annotated with:
+
+* `@type {name}`; the type of this parameter
+* `@required`; if the parameter is required.
+
+
+Find a nested model definition below:
+
+	/**
+	 * @model
+	 */
+	const AddressModel = {
+
+	    /**
+	     * @type {string}
+	     */
+	    street : '',
+
+	    /**
+	     * @type {number}
+	     */
+	    zipcode: 1010
+
+	}
+
+	/**
+	 * @model
+	 *
+	 * This is the empty response model
+	 */
+	const ExpectedModel = {
+
+	    /**
+	     * @type {number}
+	     * @required
+	     */
+	    age : 0,
+
+	    /**
+	     * @type {string}
+	     * @required
+	     */
+	    name: null,
+
+	    /**
+	     * @type {string[]}
+	     */
+	    lastNames : null,
+
+	    /**
+	     * @type {AddressModel}
+	     */
+	    address: null
+
+
+	};
+
+
+Notice how you can specify arrays by appending the typename with `[]`.
+
+Also note how the `AddressModel` is another complex type. The scraper recursively resolves them in the same document.
+
+
+
+## Hinting at where the documentation lives
+
+Oftentimes when initialising endpoints into ExpressJS, you simply refer to a variable that is a closure or function with the signature:
+
+	function(req, resp) {}
+
+Given the dynamic nature of Javascript, we cannot easily discover what file the function handler lives in (something we need to know if we would like JSdoc to parse a file). So, for that reason the `@fileHint` mechanism was introduced. 
+
+Inside your function, hint at where the handler actually lives:
+
+	app.get('/profile', (req, resp) => {
+		/* @fileHint: src/controllers/MyProfile.js::showProfile; */
+		return MyProfile.actionFunction(req, resp);
+	});
+
+or:
+
+	app.get('/profile', (req, resp) => {
+		/* @fileHint: src/controllers/MyProfile.js; */
+		return MyProfile.actionFunction(req, resp);
+	});
+
+
+The notation for filehint is as follows:
+
+	/* @fileHint: <filename>(::<id-name>); */
+
+The filename parameter is relative to the basepath passed into the scrapeEndpoints function (`./` in our example). 
+There is an optional parameter called `<id-name>`. If you have a file that has more than one handler inside it, you can specify the `@id <id-name>` it will go looking for. Otherwise it will find the first `@swagger` annotated jsdoc block.
+
+An example of this would be:
+
+
+	module.exports = {
+
+		/**
+		 * @id showProfile
+		 * @swagger
+		 * This bit of documentation is specific to the showProfile method
+		 */
+		function actionFunction(req, resp) {
+			
+		}
+
+		/**
+		 * @id anotherAction
+		 * @swagger
+		 * This bit of documentation is specific to the showProfile method
+		 */
+		function actionFunction2(req, resp) {
+			
+		}
+
+
+	}
+
+
---
+++ index.js
@@ -0,0 +1,542 @@
+/* 
+
+     APL 2.0 license applies 
+
+     Author: Marnix Kok <marnix@xinsolutions.co.nz>
+
+     ____                                       ____                                 
+    / ___|_      ____ _  __ _  __ _  ___ _ __  / ___|  ___ _ __ __ _ _ __   ___ _ __ 
+    \___ \ \ /\ / / _` |/ _` |/ _` |/ _ \ '__| \___ \ / __| '__/ _` | '_ \ / _ \ '__|
+     ___) \ V  V / (_| | (_| | (_| |  __/ |     ___) | (__| | | (_| | |_) |  __/ |   
+    |____/ \_/\_/ \__,_|\__, |\__, |\___|_|    |____/ \___|_|  \__,_| .__/ \___|_|   
+                        |___/ |___/                                 |_|              
+
+
+    Purpose:
+
+        To generate a swagger document for a set of express endpoints that have been
+        decorated with the appropriate tags.
+
+*/
+
+const _ = require('lodash');
+const fs = require('fs');
+const jsdoc = require('jsdoc-api');
+
+function getFile(base, hint) {
+    return fs.readFileSync(base + hint,{ encoding: 'utf8' });
+
+}
+
+function extractFileHint(func)  {
+    let raw = func.toString();
+    let start = raw.indexOf("@fileHint:");
+    if (start === -1) {
+        return null;
+    }
+    let nextSemiColon = raw.indexOf(";", start);
+    let instr = raw.substring(start, nextSemiColon);
+    let fileStart = instr.indexOf(":");
+
+    return (
+        instr
+            .substring(fileStart + 1)
+            .replace(/^\s+/, '')
+            .replace(/\s+$/, '')
+    );
+}
+
+function trim(str) {
+    return (
+        str
+            .replace(/^\s+/, '')
+            .replace(/\s+$/, '')
+    );
+
+}
+
+/**
+ * @returns {boolean} true if the typename is not a primitive
+ */
+function determineIsComplex(typeName) {
+    return !(
+        typeName == 'number' ||
+        typeName == 'boolean' ||
+        typeName == 'integer' ||
+        typeName == 'string' ||
+        typeName == 'object' ||
+        typeName == 'file'
+    );
+}
+
+let SwaggerScraper = _.extend(new function() {}, {
+
+    scrapeEndpoints : function(baseFolder, app) {
+
+        let stack = app._router.stack;
+        let withRoute = _.filter(stack, (s) => { return !!s.route });
+        let endpoints = [];
+
+        _.each(withRoute, (layer) => {
+            let route = layer.route;
+            let stack = layer.route.stack;
+
+
+            // a route can have multiple methods associated with it
+            _.each(stack || [], (routeLayer) => {
+
+                let fileHint = extractFileHint(routeLayer.handle);
+                let docId = '';
+
+                if (fileHint.indexOf("::") !== -1) {
+                    let hintSplit = fileHint.split("::");
+
+                    if (hintSplit.length !== 2) {
+                        throw new Error("File hint with document identifier should only contain 1 :: (for: " + fileHint + ")");
+                    }
+
+                    fileHint = hintSplit[0];
+                    docId = hintSplit[1];
+                }
+
+                endpoints.push({
+                    path: route.path,
+                    method: routeLayer.method,
+                    handler: routeLayer.handle,
+                    fileHint: fileHint,
+                    docId: docId,
+                    jsDoc: fileHint ? jsdoc.explainSync({files: baseFolder + fileHint}) : ''
+                });
+            })
+        });
+
+        return endpoints;
+    },
+
+    /**
+     * All the endpoints will get converted to a map
+     * @param endpoints the endpoints to convert
+     * @private
+     */
+    _allEndpointsToSwagger : function(endpoints, models) {
+        let thiz = this;
+        let pathsMap = {};
+
+        _.each(endpoints, function(endpoint) {
+
+            // make sure there is a map for the path
+            if (!pathsMap[endpoint.path]) {
+                pathsMap[endpoint.path] = {};
+            }
+
+            // add swagger information
+            pathsMap[endpoint.path][endpoint.method] = thiz._endpointToSwagger(endpoint, models);
+        });
+
+        return pathsMap;
+    },
+
+    /**
+     * Get the swagger comment node of a function description. If
+     * the `id` parameter is specified, "@id <id>" should appear in the
+     * comment block as well as the @swagger tag.
+     *
+     * @param jsDoc     is the JsDoc instance for the file
+     * @param id        is empty or has a specific swagger id
+     * @returns {*}     the node that first matches the description asked for
+     * @private
+     */
+    _getSwaggerDoc : function(jsDoc, id) {
+        return _.find(jsDoc, (el) => {
+            return (
+                el.kind === 'function' &&
+                el.comment.indexOf('@swagger') !== -1 && (
+                    id ? el.comment.indexOf("@id " + id) !== -1 : true
+                )
+            );
+        });
+    },
+
+    /**
+     * Get tags from under a jsdoc node with a certain title
+     *
+     * @param node  the jsdoc node to query
+     * @param title the tag type we're looking for
+     * @returns {tags[]} an array of tag information
+     * @private
+     */
+    _getTagsWithTitle : function(node, title) {
+        if (!node) {
+            return null;
+        }
+
+        return _.filter(node.tags, (tags) => {
+            return tags.title === title;
+        });
+    },
+
+    /**
+     * This method iterates over the "@response" tag array and is able to
+     * turn it into a swagger shaped response document.
+     *
+     * These tags are formatted like:
+     * @response <statusCode> {<typeName>} message
+     *
+     * @param responses
+     * @private
+     */
+    _responseMap : function(jsDoc, responses, models) {
+        let map = {};
+        _.each(responses, function(response) {
+            let split = response.text.split(/\s+/);
+            let code = split[0];
+            let description = trim(_.tail(split).join(" ")).replace("\n", " ");
+
+            let endBracket = description.indexOf("}");
+
+            let schemaStruct = null;
+            if (endBracket !== -1 && description.startsWith("{")) {
+
+                let typeName = description.substring(1, endBracket);
+                description = trim(description.substring(endBracket + 1));
+
+                let array = typeName.endsWith("[]");
+                if (array) {
+                    typeName = typeName.substring(0, typeName.length - 2);
+                }
+                let complex = determineIsComplex(typeName);
+
+                if (complex) {
+                    models.push({
+                        name: typeName,
+                        jsDoc: jsDoc
+                    });
+                }
+
+                if (array) {
+                    schemaStruct = {
+                        type: 'array',
+                        items: (
+                            complex? { "$ref" : "#/definitions/" + typeName }
+                                   : { "type" : typeName }
+                        )
+                    }
+                } else {
+                    schemaStruct = (
+                        complex? { "$ref" : "#/definitions/" + typeName }
+                               : { "type" : typeName }
+                    );
+                }
+            }
+
+            map[code] = {
+                description: description,
+                schema: schemaStruct
+            };
+        });
+        return map;
+    },
+
+    /**
+     * Parse a parameter description. It extracts param types and type hints. If possible
+     * the type hint is resolved from the same jsdoc.
+     *
+     * @param param
+     * @returns {*}
+     * @private
+     */
+    _parseDescription: function(jsdoc, param) {
+
+        let types = ["(query)", "(path)", "(header)", "(body)"];
+
+        if (!param.description) {
+            return null;
+        }
+
+        let foundParamType = null;
+        let required = false;
+
+        _.each(types, function(type) {
+            let typeIdx = param.description.indexOf(type);
+            if (typeIdx !== -1) {
+                foundParamType = type;
+
+                // required parameters have an asterisk before their type definition
+                required = (typeIdx !== 0 && param.description.charAt(typeIdx - 1) == '*');
+            }
+
+        });
+
+        // nothing found? skip parameter.
+        if (!foundParamType) {
+            return null;
+        }
+
+        // description = raw description - param type description
+        let description =
+            trim(
+                param.description.replace(
+                    (required? "*" : "") + foundParamType,
+                    ""
+                )
+            );
+
+        // get rid of parentheses
+        foundParamType = foundParamType.substring(1, foundParamType.length - 1);
+
+        // get the typehint from the description.
+        let array = false;
+        let closingBracket = description.indexOf("}");
+        let typeName = 'string';
+
+        // found {word} style at start of description? extract it.
+        if (param.type && param.type.names.length > 0) {
+            typeName = param.type.names[0];
+
+            // does the hint end in [] ? that means it's an array.
+            if (typeName.startsWith("Array.<")) {
+                array = true;
+                typeName = typeName.substring("Array.<".length, typeName.length - 1);
+            }
+
+        }
+
+        // return all we fond.
+        return {
+            description: description.replace(/\n/g, " "),
+            required: required,
+            array: array,
+            complex: determineIsComplex(typeName),
+            typeName: typeName,
+            paramType: foundParamType,
+        };
+    },
+
+    /**
+     * Parse the set of parameters available on the jsDoc block and interpret them appropriately
+     *
+     * @param params    an array of parameters
+     * @private
+     */
+    _parameterMap : function(jsDoc, params, models) {
+        let thiz = this;
+        return _.flatten(
+
+            _.map(params, (param) => {
+
+                let paramInfo = thiz._parseDescription(jsDoc, param);
+
+                if (!paramInfo) {
+                    return [];
+                }
+
+                let schemaStruct = null;
+                if (paramInfo.array) {
+                    schemaStruct = {
+                        type: 'array',
+                        items:
+                            paramInfo.complex? { "$ref" : "#/definitions/" + paramInfo.typeName }
+                                             : { "type" : paramInfo.typeName }
+                    }
+                } else {
+                    schemaStruct = (
+                        paramInfo.complex? { "$ref" : "#/definitions/" + paramInfo.typeName }
+                                         : { "type" : paramInfo.typeName }
+                    );
+                }
+
+                if (paramInfo.complex) {
+                    models.push({
+                        name: paramInfo.typeName,
+                        jsDoc: jsDoc
+                    });
+                }
+
+                return {
+                    name: param.name,
+                    in: paramInfo.paramType,
+                    required: paramInfo.required,
+                    description: paramInfo.description,
+                    schema: schemaStruct
+                };
+            })
+        );
+    },
+
+    /**
+     * Transform a single endpoint information
+     *
+     * @param endpoint
+     * @private
+     */
+    _endpointToSwagger : function(endpoint, models) {
+        let emptyTag = {text: ''};
+        let swaggerNode = this._getSwaggerDoc(endpoint.jsDoc, endpoint.docId) || {};
+        let getText = (tag) => { return _.isEmpty(tag) ? '' : tag[0].text.replace(/\n/g,  " ") };
+
+        let swagTag = this._getTagsWithTitle(swaggerNode, 'swagger');
+        let tagTags = this._getTagsWithTitle(swaggerNode, 'tag');
+        let responseTags = this._getTagsWithTitle(swaggerNode, 'response');
+        let endpointId = endpoint.method + "_" + endpoint.path.replace(/[^a-zA-Z]/g, '');
+
+        return {
+            summary: swaggerNode.summary,
+            tags: _.map(tagTags, function(tag){ return trim(tag.text); }),
+            id: endpointId,
+            description: getText(swagTag),
+            operationId: "",
+            parameters: this._parameterMap(endpoint.jsDoc, swaggerNode.params || [], models),
+            responses: this._responseMap(endpoint.jsDoc, responseTags || [], models),
+            deprecated: false
+        }
+    },
+
+
+    /**
+     * Transform an object with comments to a swagger definition
+     *
+     * @param model     the name of the model.
+     * @returns {*}
+     * @private
+     */
+    _transformObjectToDefinition: function(model) {
+        let thiz = this;
+        let results = [];
+
+        // find all model definitions
+        let mainDef = _.filter(model.jsDoc, (el) => (
+            (el.comment || '').indexOf('@model') !== -1) &&
+            el.name === model.name
+        );
+
+        if (mainDef.length === 0) {
+            console.error("Cannot find the definition");
+            return null;
+        }
+
+
+        let elements = _.filter(model.jsDoc, (el) => {
+            return el.longname.startsWith(model.name + ".") && el.kind === 'member'
+        });
+
+
+        let props = {};
+        let reqProps = [];
+
+        _.each(elements, (varEl) => {
+
+
+            let array = false;
+            let typeName = varEl.type.names[0];
+
+            if (typeName.startsWith("Array.<")) {
+                array = true;
+                typeName = typeName.substring("Array.<".length, typeName.length - 1);
+            }
+
+            let complex = determineIsComplex(typeName);
+            let typeSchema =
+                complex? { "$ref" : "#/definitions/" + typeName }
+                       : { "type" : typeName }
+           ;
+
+            if (array) {
+                props[varEl.name] = {
+                    type: 'array',
+                    items: typeSchema
+                }
+            }
+            else {
+                props[varEl.name] = typeSchema;
+            }
+
+            // recurse find the complex sub item type
+            if (complex) {
+                let lookingForModel = {
+                    name: typeName,
+                    jsDoc: model.jsDoc
+                };
+
+                let subModels = thiz._transformObjectToDefinition(lookingForModel) || [];
+
+                // add to results
+                _.each(subModels, (addMe) => results.push(addMe));
+            }
+
+            // is it a required field?
+            let reqTags = thiz._getTagsWithTitle(varEl, "required");
+            if (reqTags.length > 0) {
+                reqProps.push(varEl.name);
+            }
+        });
+
+
+        results.push({
+            type: "object",
+            title: model.name,
+            properties: props,
+            required: reqProps
+        });
+
+        return results;
+    },
+
+    /**
+     * Generate the complex type definitions based on the jsdocs and model names
+     * that have been added to the models array.
+     *
+     * @param models    an array with {name:, jsdoc:} structure
+     * @returns {null}
+     * @private
+     */
+    _generateDefinitions : function(models) {
+        let thiz = this;
+        let map = {};
+
+        _.each(models, (model) => {
+            let definitions = thiz._transformObjectToDefinition(model);
+            _.each(_.flatten([definitions]), (def) => {
+                map[def.title] = def;
+            })
+        });
+
+        return map;
+    },
+
+    /**
+     * Convert a set of endpoint definitions into a swagger json document.
+     *
+     * @param info          application information in swagger format
+     * @param host          the host this is based on
+     * @param basePath      the base path of the application
+     * @param endpoints     the endpoints that have been found in express js's routing layer.
+     *
+     * @returns {object} an object structured like a swagger 2.0 document.
+     */
+    toSwaggerJson : function(info, host, basePath, endpoints) {
+        let models = [];
+        let paths = this._allEndpointsToSwagger(endpoints, models);
+        let definitions = this._generateDefinitions(models) || [];
+
+        let swaggerJson = {
+            swagger: "2.0",
+            info: info,
+            host: host,
+            basePath: basePath,
+            consumes: [ "application/json" ],
+            produces: [ "application/json" ],
+            paths: paths,
+            definitions: definitions
+        };
+
+
+        console.log(JSON.stringify(swaggerJson));
+
+        return swaggerJson;
+    }
+
+
+});
+
+
+module.exports = SwaggerScraper;
